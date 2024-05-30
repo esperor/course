4cadd4d4d4b61b4874d4b2f029f20c2ecd06729d
esperor <pere_strelka@mail.ru>
2024-05-30 13:58:57 +0500

    init: set up client project

--- course.client/.eslintrc.cjs
+++ course.client/.eslintrc.cjs
@@ -6,13 +6,20 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'tailwind.config.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
