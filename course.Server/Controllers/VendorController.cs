038567a881516d005c3180cede7a0fc4ea96e210
esperor <pere_strelka@mail.ru>
2024-06-02 15:54:41 +0500

    feat: implement vendor api

---
+++ course.Server/Controllers/VendorController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using course.Server.Data;
+using course.Server.Configs.Enums;
+using course.Server.Configs;
+using course.Server.Models.Input;
+
+namespace course.Server.Controllers
+{
+    [Route("api/vendor")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VendorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/vendor
+        [HttpGet]
+        [AuthorizeAccessLevel(EAccessLevel.Administrator)]
+        public async Task<ActionResult<IEnumerable<Vendor>>> GetVendors(
+            string? searchName,
+            int offset = 0, 
+            int entries = 10)
+        {
+            IQueryable<Vendor> set = _context.Vendors;
+
+            if (searchName != null)
+                set = set.Where(v => v.Name.Contains(searchName));
+
+            return await set
+                .Skip(offset)
+                .Take(entries)
+                .ToListAsync();
+        }
+
+        // GET: api/vendor/5
+        [HttpGet("{id}")]
+        [AuthorizeAccessLevel(EAccessLevel.Administrator)]
+        public async Task<ActionResult<Vendor>> GetVendor(int id)
+        {
+            var vendor = await _context.Vendors.FindAsync(id);
+
+            if (vendor == null)
+            {
+                return NotFound();
+            }
+
+            return vendor;
+        }
+
+        // PUT: api/vendor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [AuthorizeAccessLevel(EAccessLevel.Administrator)]
+        public async Task<IActionResult> PutVendor(int id, Vendor vendor)
+        {
+            if (id != vendor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vendor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VendorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/vendor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [AuthorizeAccessLevel(EAccessLevel.Administrator)]
+        public async Task<ActionResult<Vendor>> PostVendor(VendorInputModel model)
+        {
+            var entry = _context.Vendors.Add(model.ToEntity());
+            await _context.SaveChangesAsync();
+            
+            return CreatedAtAction("GetVendor", new { id = entry.Entity.Id }, entry.Entity);
+        }
+
+        // DELETE: api/vendor/5
+        [HttpDelete("{id}")]
+        [AuthorizeAccessLevel(EAccessLevel.Administrator)]
+        public async Task<IActionResult> DeleteVendor(int id)
+        {
+            var vendor = await _context.Vendors.FindAsync(id);
+            if (vendor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vendors.Remove(vendor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VendorExists(int id)
+        {
+            return _context.Vendors.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ course.Server/Models/Input/VendorInputModel.cs
@@ -0,0 +1,27 @@
+﻿using course.Server.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace course.Server.Models.Input
+{
+    public class VendorInputModel
+    {
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string ContactInfo { get; set; }
+
+        [Required]
+        public required string ContractNumber { get; set; }
+
+        public Vendor ToEntity()
+        {
+            return new Vendor
+            {
+                Name = Name,
+                ContactInfo = ContactInfo,
+                ContractNumber = ContractNumber
+            };
+        }
+    }
+}
