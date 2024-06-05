6a53e3bf4bdcf0d713304d629ab0d2a3159d9d5a
esperor <pere_strelka@mail.ru>
2024-06-05 22:38:37 +0500

    ref: move logic to main.tsx

--- course.client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import { useEffect, useState } from 'react';
-
-const App = () => {}
-
-export default App;
--- course.client/src/main.tsx
+++ course.client/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import './tailwind.css';
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
