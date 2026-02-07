211219cd86999d16e844e730feee645edb89e278
esperor <pere_strelka@mail.ru>
2026-02-07 18:19:31 +0500

    chore: update router-devtools package

--- course.client/src/main.tsx
+++ course.client/src/main.tsx
@@ -18,6 +18,16 @@ declare module '@tanstack/react-router' {
 }
 
 const queryClient = new QueryClient();
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__:
+      import("@tanstack/query-core").QueryClient;
+  }
+}
+
+// This code is for all users
+window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
